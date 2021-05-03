cc4329d729ae2cf3f1c04bba6f1b031131d2ecc9
ASUS <it19116570@my.sliit.lk>
2021-05-03 21:45:14 +0530

    MY COMMIT-SHEEMA

---
+++ app/src/main/java/com/sliit/medhub/EditPayment_Info.java
@@ -0,0 +1,14 @@
+package com.sliit.medhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditPayment_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_payment__info);
+    }
+}
---
+++ app/src/main/java/com/sliit/medhub/SearchAppointment.java
@@ -0,0 +1,14 @@
+package com.sliit.medhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchAppointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_appointment);
+    }
+}
