51de1b938be078d9892bf0ddd7ff7b1a77a868b2
ASUS <it19116570@my.sliit.lk>
2021-05-05 11:26:10 +0530

    crud start

---
+++ app/src/main/java/com/sliit/medhub/Payment.java
@@ -0,0 +1,102 @@
+package com.sliit.medhub;
+
+public class Payment {
+
+    private String PatientName;
+    private String phoneNumber;
+    private String NIC;
+    private String Age;
+    private String Gender;
+    private String CardType;
+    private String CardNumber;
+    private String NameOnCard;
+    private String ExpDate;
+    private String CVC;
+
+
+    public String getPatientName() {
+        return PatientName;
+    }
+
+    public void setPatientName(String patientName) {
+        PatientName = patientName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNIC() {
+        return NIC;
+    }
+
+    public void setNIC(String NIC) {
+        this.NIC = NIC;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String age) {
+        Age = age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public String getCardType() {
+        return CardType;
+    }
+
+    public void setCardType(String cardType) {
+        CardType = cardType;
+    }
+
+    public String getCardNumber() {
+        return CardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        CardNumber = cardNumber;
+    }
+
+    public String getNameOnCard() {
+        return NameOnCard;
+    }
+
+    public void setNameOnCard(String nameOnCard) {
+        NameOnCard = nameOnCard;
+    }
+
+    public String getExpDate() {
+        return ExpDate;
+    }
+
+    public void setExpDate(String expDate) {
+        ExpDate = expDate;
+    }
+
+    public String getCVC() {
+        return CVC;
+    }
+
+    public void setCVC(String CVC) {
+        this.CVC = CVC;
+    }
+
+
+
+
+
+
+}
--- app/src/main/java/com/sliit/medhub/welcomepage.java
+++ app/src/main/java/com/sliit/medhub/welcomepage.java
@@ -11,4 +11,9 @@ public class welcomepage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcomepage);
     }
+
+
+
+
+
 }
